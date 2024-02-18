111a350fdeb7dfcba5d651a54e9bcf05e34367e1
Jodan <jacobusgrobler01@gmail.com>
2024-02-18 10:38:40 +0200

    Added solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,18 @@
 const repeatString = function(string, num) {
-    if (num <= -1) {
-        return "ERROR"
-    }else if (num < 1) {
-        return ""
-    }
-     return string.repeat(num);
+    // if (num <= -1) {
+    //     return "ERROR"
+    // }else if (num < 1) {
+    //     return ""
+    // }
+    //  return string.repeat(num);
+
+    //Their solution
+     if (num < 0) return "ERROR";
+  let stringRepeat = "";
+  for (let i = 0; i < num; i++) {
+    stringRepeat += string;
+  }
+  return stringRepeat;
 };
 
 // Do not edit below this line
