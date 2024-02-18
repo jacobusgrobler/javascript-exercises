fb8e5d82c9bb752856f642b307b6086107ad4202
Jodan <jacobusgrobler01@gmail.com>
2024-02-18 10:23:41 +0200

    Repeat String Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num <= -1) {
+        return "ERROR"
+    }else if (num < 1) {
+        return ""
+    }
+     return string.repeat(num);
 };
 
 // Do not edit below this line
