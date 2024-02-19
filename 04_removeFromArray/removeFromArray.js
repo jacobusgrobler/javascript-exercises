e1d3bb51f3a1d72af8c895460a05dbc72eccf998
Jodan <jacobusgrobler01@gmail.com>
2024-02-19 08:49:55 +0200

    Remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,41 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  
+  const newArray = [];
+ 
+  array.forEach((item) => {
+   
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  // and return that array
+  return newArray;
+  
+  
+  
+  
+  
+  // const nextArray = [];
+  
+  // array.forEach((item) => {
+  //   if (!args.includes(item)) {
+  //     nextArray.push(item)
+  //   }
+  //   return nextArray;
+  // })
+   
+  
+  /*
+  let arrFlat = arr.flat();
 
+  for (let i = 0; i < arrFlat.length; i++) {
+    arrRemover = []
+    if (i === 3) {
+      arrRemover += arrFlat.push(i)
+    }
+    return arrRemover;
+  }
+*/
 };
 
 // Do not edit below this line
