820e2834112ba4dcd7d991bf246c4a03924e6b4b
Jodan <jacobusgrobler01@gmail.com>
2024-02-19 10:40:47 +0200

    Chaleng 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    sum = null;
+//guard statements to ensure input data is valid
+if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR"
+if (a < 0 || b < 0) return "ERROR"
+if (a > b) {[a, b] = [b, a]}; //this is the swapzies statement
 
+//for loop to iterate through and += assignment operator to add iteration to sum
+for (let i = a; i <= b; i++){
+    sum += i;
+ }
+ return sum;
 };
 
 // Do not edit below this line
